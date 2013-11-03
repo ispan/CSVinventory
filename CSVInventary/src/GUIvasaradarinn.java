c8d0b455d78ca6f5d98d21aaf4addefe7fbb0294
sergeyse <sergiy@simnet.is>
2013-11-03 11:40:00 +0000

    Added sound and changed size .

--- CSVInventary/src/GUIvasaradarinn.java
+++ CSVInventary/src/GUIvasaradarinn.java
@@ -1,4 +1,5 @@
 //import java.awt.Color;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
@@ -8,6 +9,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.*;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 /*import java.io.BufferedReader;
  import java.io.FileInputStream;
  import java.io.IOException;
@@ -34,15 +42,26 @@ public class GUIvasaradarinn extends JFrame {
 	JButton bLeita = new JButton("Leita");
 	String msgText;
 
+	private AudioInputStream audioInputStream;
+
+	private AudioFormat audioFormat;
+
+	private SourceDataLine sourceDataLine;
+	public boolean stopPlayback = false;
+	
+
 	// JButton bDelete = new JButton("Fjarlaegja");
 
 	public GUIvasaradarinn() {
 		super("Vasaradarinn 1.9");
 		lpontunarNumer = new JLabel(" Pontunar N: ");
-		lpontunarNumer.setFont(new Font("Courier New", Font.ITALIC, 20));
+		lpontunarNumer.setFont(new Font("Courier New", Font.ITALIC, 40));
 		// lpontunarNumer.setForeground(Color.GRAY);
+		txtPontunarNumer.setFont(new Font("Helvetica", Font.BOLD, 60));
 		JLabel lblVasaNumer = new JLabel(" I Vasanum N :");
-		lblVasaNumer.setFont(new Font("Courier New", Font.ITALIC, 20));
+		lblVasaNumer.setFont(new Font("Courier New", Font.ITALIC, 40));
+		bLeita.setFont(new Font("Courier New", Font.ITALIC, 40));
+		bLeita.setBackground(Color.GREEN);
 		GridLayout gr = new GridLayout(3, 2, 1, 1);
 		setLayout(gr);
 
@@ -118,18 +137,24 @@ public class GUIvasaradarinn extends JFrame {
 			
 			// change UI for sales department , make DELETE option unavalable.
 			
-			
+			playAudio();
 			int n = JOptionPane.showConfirmDialog(null, "I Vasa No. " + "\n"
 
 			+ msgText + "\n" + " Delete?", null, JOptionPane.YES_NO_OPTION);
 			if (n == JOptionPane.YES_OPTION) {
 				// StackReader sr = new StackReader();
 				sr.DeletePontun(txtPontunarNumer.getText());
+				stopPlayback=true;
+			
 
 				txtPontunarNumer.setText("");
 				txtFetchedVasaN.setText("");
 			} else if (n == JOptionPane.NO_OPTION) {
-
+				
+				stopPlayback=true;
+			}else if (n == JOptionPane.CLOSED_OPTION){
+				
+				stopPlayback = true;
 			}
 
 		} catch (NumberFormatException e) {
@@ -140,6 +165,94 @@ public class GUIvasaradarinn extends JFrame {
 		}
 
 	}
+	  private void playAudio() {
+		    try{
+		      File soundFile =
+		                   new File("w.wav");
+		      audioInputStream = AudioSystem.
+		                  getAudioInputStream(soundFile);
+		      audioFormat = audioInputStream.getFormat();
+		      System.out.println(audioFormat);
+
+		      DataLine.Info dataLineInfo =
+		                          new DataLine.Info(
+		                            SourceDataLine.class,
+		                                    audioFormat);
+
+		      sourceDataLine =
+		             (SourceDataLine)AudioSystem.getLine(
+		                                   dataLineInfo);
+
+		      //Create a thread to play back the data and
+		      // start it running.  It will run until the
+		      // end of file, or the Stop button is
+		      // clicked, whichever occurs first.
+		      // Because of the data buffers involved,
+		      // there will normally be a delay between
+		      // the click on the Stop button and the
+		      // actual termination of playback.
+		      new PlayThread().start();
+		    }catch (Exception e) {
+		      e.printStackTrace();
+		      System.exit(0);
+		    }//end catch
+		  }//end playAudio
+	  
+	//=============================================//
+	//Inner class to play back the data from the
+	// audio file.
+	class PlayThread extends Thread{
+		byte tempBuffer[] = new byte[1000000];
+
+	
+
+	  public void run(){
+		
+	    try{
+	      sourceDataLine.open(audioFormat);
+	      sourceDataLine.start();
+
+	      int cnt;
+	      //Keep looping until the input read method
+	      // returns -1 for empty stream or the
+	      // user clicks the Stop button causing
+	      // stopPlayback to switch from false to
+	      // true.
+	      
+	      // stopPlayback is in an inner class , another thread
+	    
+	      while((cnt = audioInputStream.read(
+	           tempBuffer,0,tempBuffer.length)) != -1
+	                     //  && stopPlayback == false){
+	                                                 ){
+	        if(cnt > 0){
+	          //Write data to the internal buffer of
+	          // the data line where it will be
+	          // delivered to the speaker.
+	        	while (stopPlayback == false ){
+	          sourceDataLine.write(
+	                             tempBuffer, 0, cnt);
+	        sleep (1000);
+	        	}// here ends our infinity loop
+	        }//end if
+	      }//end while
+	      //Block and wait for internal buffer of thes
+	      // data line to empty.
+	  
+	      sourceDataLine.drain();
+	      sourceDataLine.close();
+
+	      //Prepare to playback another file
+	   /*   stopBtn.setEnabled(false);
+	      playBtn.setEnabled(true);
+	      stopPlayback = false;*/
+	    }catch (Exception e) {
+	      e.printStackTrace();
+	      System.exit(0);
+	    }//end catch
+	
+	  }//end run
+	}//end inner class PlayThread
 
 	public static void main(String args[]) {
 		try {
@@ -149,7 +262,8 @@ public class GUIvasaradarinn extends JFrame {
 		} catch (Exception e) {
 		}
 		GUIvasaradarinn gvr = new GUIvasaradarinn();
-		gvr.setSize(400, 150);
+		gvr.setSize(800, 400);
 		gvr.setVisible(true);
+	
 	}
 }
